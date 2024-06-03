5d78f0a981e0ad2d5f85b2ddbe4bbb37645fbe11
Chase <cefortner@gmail.com>
2024-06-02 21:18:34 -0400

    update

--- main.go
+++ main.go
@@ -9,19 +9,19 @@ import (
 	"gonum.org/v1/gonum/stat"
 )
 
-func generateSample(sampleSize int, mean, sd float64, rng *rand.Rand) []float64 {
+func generateSample(sampleSize int, mean, sd float64, randNumGen *rand.Rand) []float64 {
 	sample := make([]float64, sampleSize)
 	for i := 0; i < sampleSize; i++ {
-		sample[i] = rng.NormFloat64()*sd + mean
+		sample[i] = randNumGen.NormFloat64()*sd + mean
 	}
 	return sample
 }
 
 func bootstrapSample(data []float64, rng *rand.Rand) []float64 {
-	n := len(data)
-	resample := make([]float64, n)
+	data_len := len(data)
+	resample := make([]float64, data_len)
 	for i := range resample {
-		resample[i] = data[rng.Intn(n)]
+		resample[i] = data[rng.Intn(data_len)]
 	}
 	return resample
 }
@@ -32,19 +32,18 @@ func main() {
 	popMean := 100.0
 	popSD := 10.0
 
-	rng := rand.New(rand.NewSource(9999)) // For reproducible results
+	randNumGen := rand.New(rand.NewSource(9999))
 
-	// Generate a single sample
-	thisSample := generateSample(sampleSize, popMean, popSD, rng)
+	thisSample := generateSample(sampleSize, popMean, popSD, randNumGen)
 
 	bootstrapMeans := make([]float64, B)
 	bootstrapMedians := make([]float64, B)
 
 	for b := 0; b < B; b++ {
-		thisBootstrapSample := bootstrapSample(thisSample, rng)
-		sort.Float64s(thisBootstrapSample)
-		bootstrapMeans[b] = stat.Mean(thisBootstrapSample, nil)
-		bootstrapMedians[b] = stat.Quantile(0.5, stat.Empirical, thisBootstrapSample, nil)
+		BootSample := bootstrapSample(thisSample, randNumGen)
+		sort.Float64s(BootSample)
+		bootstrapMeans[b] = stat.Mean(BootSample, nil)
+		bootstrapMedians[b] = stat.Quantile(0.5, stat.Empirical, BootSample, nil)
 	}
 
 	seMeanCLT := popSD / math.Sqrt(float64(sampleSize))
